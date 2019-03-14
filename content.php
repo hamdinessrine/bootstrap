1dbde6c068de6a0686b7a0ecd5a6ee59bbefb680
Unknown <hamdinessrin9@gmail.com>
2019-03-14 12:13:45 +0100

    modifier index

---
+++ content.php
@@ -0,0 +1,6 @@
+<div class="blog-post">
+            <h2 class="blog-post-title"><?php the_title();?></h2>
+            <p class="blog-post-meta"><?php the_date();?> <a href="#"><? php the_auther();?></a></p>
+
+             <?php the_content(); ?> 
+          </div><!-- /.blog-post -->
